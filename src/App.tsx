ab067a6818f4cb25df5299d9dfc86f52b912378b
John <johnoneil.tech@gmail.com>
2026-02-11 13:00:00 -0500

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router";
+import Gallery from "./pages/Gallery";
+import Details from "./pages/Details";
+import Favorites from "./pages/Favorites";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Gallery</h1>} />
-          <Route path="/comic/:id" element={<h1>Comic Details page</h1>} />
-          <Route path="/favorites" element={<h1>Favorites page</h1>} />
+          <Route path="/" element={<Gallery />} />
+          <Route path="/comic/:id" element={<Details />} />
+          <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/Gallery.tsx
@@ -0,0 +1,8 @@
+export default function Gallery() {
+  return (
+    <main style={{ padding: "1rem" }}>
+      <h1>Gallery</h1>
+      <p>Home page for the comic gallery.</p>
+    </main>
+  );
+}
